4cf7d8352e69e373289e7b7bf973eca6d2691202
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-01-06 00:45:17 +0700

    Initial Spring

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+#FROM gradle:6.7-jre15
+FROM openjdk:15-alpine
+RUN addgroup -S spring && adduser -S spring -G spring
+USER spring:spring
+ARG JAR_FILE=build/libs/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ src/main/java/id/my/berviantoleo/boilerplate/BoilerplateApplication.java
@@ -0,0 +1,21 @@
+package id.my.berviantoleo.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
